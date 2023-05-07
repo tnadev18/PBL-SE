13a70036405d2251338f630f41336c42904d5ff2
Vedant Badre <vedantbadre18@gmail.com>
2023-05-07 19:55:25 +0530

    frontend

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,7 +1,20 @@
 import '@/styles/globals.css'
-// _app.js
 import 'bootstrap/dist/css/bootstrap.css'
 import 'tailwindcss/tailwind.css'
+import '@/styles/common.css'
+import '@/styles/facebook-menu.css'
+import '@/styles/icons.css'
+import '@/styles/navigation.css'
+import '@/styles/reset.css'
+import '@/styles/right-column.css'
+import '@/styles/style.css'
+import '@/styles/system-tokens.css'
+import '@/styles/create-room.css'
+import '@/styles/feed.css'
+import '@/styles/grid.css'
+import '@/styles/new-post.css'
+import '@/styles/stories.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,10 @@
 import Navbar from "@/Components/Navbar"
+import Image from "next/image"
 
 export default function Index(){
   return (
-    <>
+    <main>
     <Navbar/>
-    </>
+    </main>
   )
 }
