8007cd71d48d49bfac14586705d474a8a46deab3
Vedant Badre <vedantbadre18@gmail.com>
2023-05-11 21:23:34 +0530

    new post window

---
+++ frontend/src/Components/NewPostWindow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewPostWindow = () => {
+  return (
+    <>
+    <div className="new-post-window">
+        
+    </div>
+    </>
+  )
+}
+
+export default NewPostWindow
